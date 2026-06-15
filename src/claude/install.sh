d8db2e12198c3500e531d591625a4396ae3d23c2
Tony Valderrama <406656+tvald@users.noreply.github.com>
2026-06-15 23:51:27 +0900

    fix claude global binary softlink

--- src/claude/install.sh
+++ src/claude/install.sh
@@ -11,6 +11,8 @@ apt-get -y install --no-install-recommends ca-certificates curl  >/dev/null
 # https://downloads.claude.ai/claude-code-releases/bootstrap.sh
 su "$_REMOTE_USER" -c "'$(dirname "$0")/bootstrap.sh' '$VERSION'"
 
+ln -s "/home/$_REMOTE_USER/.local/bin/claude" /usr/local/bin/claude
+
 # clean up apt-get
 apt-get clean  >/dev/null
 rm -rf /var/lib/apt/lists/*  >/dev/null
