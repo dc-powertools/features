17614e57517d6945d1fbbb9f21b3b6747f3e9420
Tony Valderrama <406656+tvald@users.noreply.github.com>
2026-06-17 11:59:46 +0900

    fix issue with path mangling in rust feature

--- src/rust/install.sh
+++ src/rust/install.sh
@@ -30,6 +30,10 @@ fi
 mkdir -p /usr/local/share/rust
 cp "$(dirname "$0")/onCreate.sh" /usr/local/share/rust/onCreate.sh
 
+cat > /etc/profile.d/rust.sh <<'EOF'
+export PATH="${CARGO_HOME}/bin:${PATH}"
+EOF
+
 chown -R "$_REMOTE_USER:$_REMOTE_USER" "$RUSTUP_HOME" "$CARGO_HOME"
 
 rustc --version
