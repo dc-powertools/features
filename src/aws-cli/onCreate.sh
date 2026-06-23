8bace7feddfb2cf9428952504e781e39c73c644a
Tony Valderrama <406656+tvald@users.noreply.github.com>
2026-06-23 15:36:25 +0900

    fix aws cli config mount

--- src/aws-cli/onCreate.sh
+++ src/aws-cli/onCreate.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/bash
 set -e
 
-if [ -z "$AWS_CONFIG_FILE" ]; then
-    exit 0
-fi
-
-mkdir -p "$(dirname "$AWS_CONFIG_FILE")"
-
+CONFIG_FILE="~/.aws/config"
 SEED="/usr/local/share/aws-cli/config"
-if [ -f "$SEED" ] && [ ! -f "$AWS_CONFIG_FILE" ]; then
-    cp "$SEED" "$AWS_CONFIG_FILE"
+
+if [ -f "$SEED" ] && [ ! -f "$CONFIG_FILE" ]; then
+    cp "$SEED" "$CONFIG_FILE"
 fi
